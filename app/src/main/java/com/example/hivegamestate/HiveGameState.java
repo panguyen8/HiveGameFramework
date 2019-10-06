e764e41553d56a5a97cee46c82a20201b2405206
liuer21 <54870441+liuer21@users.noreply.github.com>
2019-10-06 12:05:58 -0700

    Instance variables for HiveGameState were created

---
+++ app/src/main/java/com/example/hivegamestate/HiveGameState.java
@@ -0,0 +1,11 @@
+package com.example.hivegamestate;
+
+public class HiveGameState {
+    static int turn;
+
+    static String[][] board = new String[100][100];
+
+    static int wBee, wSpider, wAnt, wBeetle, wGrasshopper;
+    static int bBee, bSpider, bAnt, bBeetle, bGrasshopper;
+
+}
