a00cd9145b8e5aa65d063dd3afa9930f25e64520
liuer21 <54870441+liuer21@users.noreply.github.com>
2019-10-06 12:16:24 -0700

    Instance variables for HiveGameState were created and annotated.

--- app/src/main/java/com/example/hivegamestate/HiveGameState.java
+++ app/src/main/java/com/example/hivegamestate/HiveGameState.java
@@ -1,11 +1,20 @@
 package com.example.hivegamestate;
 
 public class HiveGameState {
+    //static variable to tell whose turn it is
     static int turn;
 
+    static final int BLACK_TURN = 0;
+    static final int WHITE_TURN = 1;
+
+    //holds a 2d array of strings for what the board would look like
+    //using a 2d array for now, idk if we will used a linked list later on
+    //once we get the class for the pieces set up, we can change string to that new class
     static String[][] board = new String[100][100];
 
+    //holds the value for how many pieces the white and black player have in their hand (not on board)
     static int wBee, wSpider, wAnt, wBeetle, wGrasshopper;
     static int bBee, bSpider, bAnt, bBeetle, bGrasshopper;
+
 
 }
