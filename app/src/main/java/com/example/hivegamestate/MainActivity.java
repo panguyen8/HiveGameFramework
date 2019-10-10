88fe5a857ce5cfb6c7a43e9acc8a3826fbace865
Phuocan Nguyen <phuocan.nguyen8@gmail.com>
2019-10-09 17:37:17 -0700

    Phuocan - only part of OnClickListener(Missing method calls), Constructor, CopyConstructor, and a Check Class(to help with OnClickListener)

--- app/src/main/java/com/example/hivegamestate/MainActivity.java
+++ app/src/main/java/com/example/hivegamestate/MainActivity.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity{
+
+    final EditText theText = findViewById(R.id.theEditText);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,44 @@ public class MainActivity extends AppCompatActivity {
         HiveGameState hiveGameState = new HiveGameState();
 
         //initialize the the Run Test button
-        View runTestButton = findViewById(R.id.runTestButton);
+        Button runTestButton = findViewById(R.id.runTestButton);
+
+        runTestButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //clears the text once clicked
+                theText.setText("");
+
+                //the first instance of the game created
+                HiveGameState firstInstance = new HiveGameState();
+
+                //second instance created by deep copy from first instance
+                HiveGameState secondInstance = new HiveGameState(firstInstance);
+
+                //call all methods of firstInstance
+
+                //each method requires a String to be appended to the editText object
+                theText.append("randomString");
+
+                //the third instance of game created
+                HiveGameState thirdInstance = new HiveGameState();
+
+                //fourth instance deep copy made from the third instance
+                HiveGameState fourthInstance = new HiveGameState(thirdInstance);
+
+                //append second and fourth state strings to theText if both are equal
+                if (check(secondInstance, fourthInstance)) {
+                    theText.append(secondInstance.toString());
+                    theText.append(fourthInstance.toString());
+                }
+            }
+        });
+
+    }
+
+    public Boolean check(HiveGameState stateOne, HiveGameState stateTwo) {
+        if(stateOne.toString() == stateTwo.toString()) {
+            return true;
+        }
+        return false;
     }
 }
